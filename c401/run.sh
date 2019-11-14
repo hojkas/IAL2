db09bc6eece2af2888579d53c600bacb08760ce5
hojkas <ivka.strnadova@gmail.com>
2019-11-14 20:29:33 +0100

    test

---
+++ c401/run.sh
@@ -0,0 +1,9 @@
+clear
+clear
+make
+echo "test begin
+"
+./c401-test
+echo "test end
+"
+make clean
---
+++ c402/run.sh
@@ -0,0 +1,9 @@
+clear
+clear
+make
+echo "test begin
+"
+./c402-test
+echo "test end
+"
+make clean
